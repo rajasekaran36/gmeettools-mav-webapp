bf9ade16e592e08a0513b7dd4ca54377891e94d7
Rajasekaran S <proffraja@gmail.com>
2021-06-03 17:39:43 +0530

    first commit

---
+++ src/main/java/Process.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class Process extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("Hello");
+    }
+}
